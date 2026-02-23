fced8981a93fb7a5759bb182ff4e654ff2373f38
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-23 11:19:02 -0300

    Corregi la ruta para acceder al prismaClient y agregue un controlador para agregar un nuevo producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,5 +1,19 @@
 import { json } from "express";
+import { prisma } from "../server/prisma.js";
 
 export const agregarProducto = async (req, res) => {
-  res.send("Desde el controlador agregar producto");
+  try {
+    const nuevoProducto = await prisma.producto.create({
+      data: req.body,
+    });
+    res.status(201).json({
+      mensaje: "Producto agregado exitosamente",
+      producto: nuevoProducto,
+    });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error al agregar el producto" });
+  }
 };
